dff65d27889ce13140c6b4c7e9645740f360178c
dhruvishasatani <dhruvishasatani123@gmail.com>
2025-01-02 22:04:52 +0530

    backend connect signup and signin

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,9 +1,9 @@
-const e = require('express');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const USER = mongoose.model("USER");
 const bcrypt = require('bcrypt');
+// const jwt = require("jsonwebtoken");
 
 router.get('/', (req, res) => {
     res.send("Hello World");
@@ -42,6 +42,29 @@ router.post('/signup', (req, res) => {
     // res.json({message: "Successfully posted"});
 
    
+});
+
+router.post('/signin', (req, res) => {
+    const{email,password} = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add all the fields" })
+    }
+
+    USER.findOne({email:email}).then((savedUser) => {
+        if (!savedUser) {
+            return res.status(422).json({ error: "Invalid Email or Password" })
+        }
+        console.log(savedUser);
+        
+        bcrypt.compare(password, savedUser.password).then((doMatch) => {
+            if (doMatch) {
+                res.json({message: "Successfully signed in"})
+            } else {
+                return res.status(422).json({ error: "Invalid Email or Password" })
+            }
+        })
+        .catch(err => console.log(err))
+    })
 });
 
 
--- frontend/src/Components/SignIn.jsx
+++ frontend/src/Components/SignIn.jsx
@@ -1,39 +1,118 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState,useContext } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { toast} from 'react-toastify';
+// import { LoginContext } from "../Context/LoginContext";
 
 const SignIn = () => {
+  // const { setUserLogin } = useContext(LoginContext)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  
+  // Toast functions
+  const notifyA = (msg) => toast.error(msg)
+  const notifyB = (msg) => toast.success(msg)
+
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+  const postData = () => {
+    //checking email
+    if (!emailRegex.test(email)) {
+      notifyA("Invalid email")
+      return
+    }
+    // Sending data to server
+    fetch("http://localhost:5000/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+
+      })
+    }).then(res => res.json())
+      .then(data => {
+        if (data.error) {
+          notifyA(data.error)
+        } else {
+          notifyB("Signed In Successfully")
+          
+          // localStorage.setItem("jwt", data.token)
+          // localStorage.setItem("user", JSON.stringify(data.user))
+
+          // setUserLogin(true)
+          navigate("/")
+        }
+        console.log(data)
+      })
+  }
+
+
+
   return (
-    <div  className="bg-cover bg-no-repeat w-full flex justify-center items-center flex-1 h-svh fixed  "
-    style={{
-      backgroundImage:
-        "url(https://lh5.googleusercontent.com/proxy/70IQor00Qaw4GaR5wj0DQw8YGbUAhlMxbXS2UxY3I8WEFBtm7vVwfTo8fw70PrOyKybV83XDlzLnBcUV8XMxI4TM4-11WVEztK-eRnEyxf29LHfpWD5CXWDX26qVrUPk0tMkWe1cnMmCInTi_A)",
-    }}>
-      <div className='container mx-auto px-4 flex justify-center items-center h-full '>
-           <div className="flex flex-col justify-center items-center  backdrop-blur-sm bg-white/30  shadow-2xl px-[30px] py-[50px] w-96 rounded-xl  space-y-4">
-           <img
+    <div
+      className="bg-cover bg-no-repeat w-full flex justify-center items-center flex-1 h-svh fixed  "
+      style={{
+        backgroundImage:
+          "url(https://lh5.googleusercontent.com/proxy/70IQor00Qaw4GaR5wj0DQw8YGbUAhlMxbXS2UxY3I8WEFBtm7vVwfTo8fw70PrOyKybV83XDlzLnBcUV8XMxI4TM4-11WVEztK-eRnEyxf29LHfpWD5CXWDX26qVrUPk0tMkWe1cnMmCInTi_A)",
+      }}
+    >
+      <div className="container mx-auto px-4 flex justify-center items-center h-full ">
+        <div className="flex flex-col justify-center items-center  backdrop-blur-sm bg-white/30  shadow-2xl px-[30px] py-[50px] w-96 rounded-xl  space-y-4">
+          <img
             src="https://github.com/Abhisek730/Instagram-mern-stack/blob/main/frontend/src/img/logo.png?raw=true"
             alt=""
             className="w-52"
           />
-            <div className="w-full">
-            <input type="email" placeholder="email@gmail.com" name="email" className="border border-gray-400 p-[5px] placeholder:text-gray-300  rounded-lg w-full outline-none" />
+          <div className="w-full">
+            <input
+              type="email"
+              value={email}
+              id="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="email@gmail.com"
+              name="email"
+              className="border border-gray-400 p-[5px] placeholder:text-gray-300  rounded-lg w-full outline-none"
+            />
           </div>
           <div className="w-full">
-            <input type="password" placeholder="Password" name="password" className="border border-gray-400 p-[5px] placeholder:text-gray-300  rounded-lg w-full outline-none" />
+            <input
+              type="password"
+              value={password}
+              id="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="Password"
+              name="password"
+              className="border border-gray-400 p-[5px] placeholder:text-gray-300  rounded-lg w-full outline-none"
+            />
           </div>
-          <input type="submit" id="submit-btn" value="Sign In" className="bg-blue-500 p-[6px] placeholder:text-gray-300  rounded-lg w-full outline-none cursor-pointer  text-white font-semibold text-lg "  />  
+          <input
+            type="submit"
+           id="login-btn" 
+           onClick={() => { postData() }} 
+           value="Sign In"
+            className="bg-blue-500 p-[6px] placeholder:text-gray-300  rounded-lg w-full outline-none cursor-pointer  text-white font-semibold text-lg "
+          />
           <hr className="w-full border border-gray-300" />
 
           <div className="mt-3">
-          Don't have an account ?
-          <Link to="/signup">
-            <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span>
-          </Link>
+            Don't have an account ?
+            <Link to="/signup">
+              <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span>
+            </Link>
+          </div>
         </div>
-           </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
